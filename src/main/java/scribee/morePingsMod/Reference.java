793fa327bbb28c2bb7b43bc38f164b800eabe686
Scribee <38041006+Scribee@users.noreply.github.com>
2018-04-16 19:36:46 -0600

    Added GUI_FACTORY

--- src/main/java/scribee/morePingsMod/Reference.java
+++ src/main/java/scribee/morePingsMod/Reference.java
@@ -1,7 +1,8 @@
 package scribee.morePingsMod;
 
 public class Reference {
-	public static final String MOD_ID = "mp";
+	public static final String MODID = "mp";
 	public static final String MOD_NAME = "More Pings Mod";
 	public static final String VERSION = "1.0";
+	public static final String GUI_FACTORY = "scribee.morePingsMod.MorePingsGuiFactory";
 }
