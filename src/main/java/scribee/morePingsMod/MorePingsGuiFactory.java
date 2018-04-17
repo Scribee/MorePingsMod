c87d51cb75c981b909c4be1bacd1155858274ad6
Scribee <38041006+Scribee@users.noreply.github.com>
2018-04-16 19:37:51 -0600

    Added config GUI factory

---
+++ src/main/java/scribee/morePingsMod/MorePingsGuiFactory.java
@@ -0,0 +1,31 @@
+package scribee.morePingsMod;
+
+import java.util.Set;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.fml.client.IModGuiFactory;
+
+public class MorePingsGuiFactory implements IModGuiFactory {
+
+	@Override
+	public void initialize(Minecraft minecraftInstance) {
+	
+	}
+
+	@Override
+	public Class<? extends GuiScreen> mainConfigGuiClass() {
+		return GuiMorePingsConfig.class;
+	}
+
+	@Override
+	public Set<RuntimeOptionCategoryElement> runtimeGuiCategories() {
+		return null;
+	}
+
+	@Override
+	public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element) {
+		return null;
+	}
+
+}
