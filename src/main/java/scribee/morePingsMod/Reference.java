e428654589e3dbf1228c8b5ae3974dc3f4202e50
Scribee <38041006+Scribee@users.noreply.github.com>
2018-04-21 22:18:53 -0600

    Fixed gui factory path

--- src/main/java/scribee/morePingsMod/Reference.java
+++ src/main/java/scribee/morePingsMod/Reference.java
@@ -3,6 +3,6 @@ package scribee.morePingsMod;
 public class Reference {
 	public static final String MODID = "mp";
 	public static final String MOD_NAME = "More Pings Mod";
-	public static final String VERSION = "1.0";
-	public static final String GUI_FACTORY = "scribee.morePingsMod.MorePingsGuiFactory";
+	public static final String VERSION = "1.1";
+	public static final String GUI_FACTORY = "scribee.morePingsMod.gui.MorePingsGuiFactory";
 }
