733d36b26a68e941b02c64f0c8606ff501d33a68
Scribee <38041006+Scribee@users.noreply.github.com>
2018-04-24 19:09:51 -0600

    Updated version number

--- src/main/java/scribee/morePingsMod/Reference.java
+++ src/main/java/scribee/morePingsMod/Reference.java
@@ -3,6 +3,6 @@ package scribee.morePingsMod;
 public class Reference {
 	public static final String MODID = "mp";
 	public static final String MOD_NAME = "More Pings Mod";
-	public static final String VERSION = "1.1.3";
+	public static final String VERSION = "1.1.5";
 	public static final String GUI_FACTORY = "scribee.morePingsMod.gui.MorePingsGuiFactory";
 }
