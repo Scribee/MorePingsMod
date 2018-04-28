72ac111820416f4ba318d6a311cb2fe27b36c30c
Scribee <38041006+Scribee@users.noreply.github.com>
2018-04-27 18:19:15 -0600

    Removed old imports

--- src/main/java/scribee/morePingsMod/gui/MorePingsGuiFactory.java
+++ src/main/java/scribee/morePingsMod/gui/MorePingsGuiFactory.java
@@ -1,13 +1,9 @@
 package scribee.morePingsMod.gui;
 
-import java.util.Collections;
 import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.client.IModGuiFactory;
 
 public class MorePingsGuiFactory implements IModGuiFactory {
